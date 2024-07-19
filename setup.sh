a7b436cdb27479e628a558ad316e8a67a0abd950
Steve D Sun <sund.chn@gmail.com>
2024-07-20 00:56:37 +0800

    add choice

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 sudo apt update -y
-sudo apt upgrade -y
+#sudo apt upgrade -y
 sudo apt install -y curl git
+
+source ~/.local/share/onekey-proxy/choices.sh
 
 for proxy in ~/.local/share/onekey-proxy/proxy/*.sh; do source $proxy; done
