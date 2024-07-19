fa08298bb3955f4802c7ddd96c86a3ee0c4387cb
Steve D Sun <sund.chn@gmail.com>
2024-07-20 01:11:15 +0800

    update gum

--- setup.sh
+++ setup.sh
@@ -1,7 +1,4 @@
-sudo apt update -y
-#sudo apt upgrade -y
-sudo apt install -y curl git
-
+source ~/.local/share/onekey-proxy/required/app-gum.sh >/dev/null
 source ~/.local/share/onekey-proxy/choices.sh
 
 for proxy in ~/.local/share/onekey-proxy/proxy/*.sh; do source $proxy; done
